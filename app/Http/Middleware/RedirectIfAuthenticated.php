624a6a3462b6ff31de2192ce2c15931e5088e933
lijinlei <lijinlei@playcrab.com>
2021-01-27 23:51:53 +0800

    登录用户跳转到首页而不是login

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,8 +17,9 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
+        //如果认证过重定向到首页
         if (Auth::guard($guard)->check()) {
-            return redirect('/login');
+            return redirect('/');
         }
 
         return $next($request);
