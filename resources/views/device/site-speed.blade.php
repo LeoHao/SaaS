4463b33a2a0a43638e7ede077383cdc82cf15f2b
lijinlei <lijinlei@playcrab.com>
2021-02-01 15:46:04 +0800

    [update]

--- resources/views/device/site-speed.blade.php
+++ resources/views/device/site-speed.blade.php
@@ -1,8 +1,20 @@
 @extends('layouts.app')
 
 @section('css')
-    <link href="{{ asset('css/dataTables.bootstrap4.css') }}" rel="stylesheet">
-    <style>
+  <link href="{{ asset('assets/plugins/range-slider/css/bootstrap-slider.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets/css/pages/rangeslider.css') }}" rel="stylesheet">
+
+  <link rel="stylesheet" href="{{ asset('assets/plugins/smart-wizard/css/smart_wizard.min.css') }}" rel="stylesheet">
+  <link rel="stylesheet" href="{{ asset('assets/plugins/smart-wizard/css/smart_wizard_theme_arrows.min.css') }}" rel="stylesheet">
+  <link rel="stylesheet" href="{{ asset('assets/plugins/smart-wizard/css/smart_wizard_theme_circles.min.css') }}" rel="stylesheet">
+  <link rel="stylesheet" href="{{ asset('assets/plugins/smart-wizard/css/smart_wizard_theme_dots.min.css') }}" rel="stylesheet">
+
+  <!-- vendor css -->
+  <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+  <!-- dark layouts -->
+  <link rel="stylesheet" href="{{ asset('assets/css/layouts/dark.css') }}">
+
+  <style>
     .custom {
         width: 200px !important;
     }
@@ -10,70 +22,93 @@
 @endsection
 
 @section('content')
-  <div class="row m-auto">
-      <div class="col-auto">
-           <button type="button" class="btn btn-lg btn-outline-warning select-checked custom" value="1">
-               <i class="fab fa-github text-dark fa-7x"></i>
-          </button>
-      </div>
+  <div class="row">
+     <!-- [ Smart-Wizard ] start -->
+      <div class="col-sm-12">
+          <div class="card">
+              <div class="card-header">
+                  <h5>站点加速</h5>
+              </div>
+              <div class="card-block">
+                <!-- [ SmartWizard html ] start -->
+                  <div id="smartwizard">
+                         <ul>
+              <li>
+                <a href="#step-l-1">
+                      <h6>选择加速站点</h6>
+                  {{--                                  <p class="m-0">选择所在地</p>--}}
+                  </a>
+              </li>
+              <li>
+                <a href="#step-l-2">
+                      <h6>选择时长</h6>
+                  {{--                                  <p class="m-0">选择目的地</p>--}}
+                  </a>
+              </li>
+              <li>
+                <a href="#step-l-3">
+                      <h6>确认</h6>
+                  {{--                                  <p class="m-0">选择带宽</p>--}}
+                  </a>
+              </li>
+          </ul>
+         <div>
+           <div id="step-l-1">
+              <h5>站点</h5>
+              <div class="form-group">
+                <button type="button" class="btn btn-lg btn-outline-warning select-checked custom" value="1">
+                  <i class="fab fa-github text-dark fa-7x"></i>
+                </button>
 
-       <div class="col-auto">
-           <button type="button" class="btn btn-lg btn-outline-warning select-checked custom" value="2">
-               <i class="fab fa-facebook-f text-primary fa-7x"></i>
-          </button>
-      </div>
+                <button type="button" class="btn btn-lg btn-outline-warning select-checked custom" value="2">
+                  <i class="fab fa-facebook-f text-primary fa-7x"></i>
+                </button>
 
-      <div class="col-auto">
-           <button type="button" class="btn btn-lg btn-outline-warning select-checked custom" value="3">
-               <i class="fab fa-youtube text-danger fa-7x"></i>
-          </button>
+                <button type="button" class="btn btn-lg btn-outline-warning select-checked custom" value="3">
+                  <i class="fab fa-youtube text-danger fa-7x"></i>
+                </button>
+              </div>
+           </div>
+           <div id="step-l-2">
+             <h5>时长</h5>
+             <div class="form-group">
+               <div class="col-md-9 col-form-label">
+                 <input id="duration" type="text" data-slider-min="1" data-slider-max="36" data-slider-step="1" data-slider-value="1">
+                  <br>
+                 <span id="ex6CurrentSliderValLabel">
+                    <span id="durationVal">1</span>个月
+                 </span>
+               </div>
+             </div>
+           </div>
+           <div id="step-l-3">
+                <h5>提交申请</h5>
+                <button class="btn btn-success" type="button" id="speed">
+                   <i class="fa fa-rocket text-danger fa-3x">提交</i>
+                </button>
+            </div>
+         </div>
+                  </div>
+              </div>
+          </div>
       </div>
+     <!-- [ Smart-Wizard ] end -->
   </div>
-  <br>
-
-  <div class="row align-items-center">
-      <div class="col-6 col-sm-4 col-md mb-3 mb-xl-0 text-center">
-          <button class="btn btn-block btn-outline-success duration" type="button" value="1">
-                <i class="fa fa-calendar text-primary fa-1x">1个月</i>
-          </button>
-      </div>
-      <div class="col-6 col-sm-4 col-md mb-3 mb-xl-0 text-center">
-          <button class="btn btn-block btn-outline-success duration" type="button" value="2">
-              <i class="fa fa-calendar text-primary fa-1x">2个月</i>
-          </button>
-      </div>
-      <div class="col-6 col-sm-4 col-md mb-3 mb-xl-0 text-center">
-          <button class="btn btn-block btn-outline-success duration" type="button" value="3">
-                <i class="fa fa-calendar text-primary fa-1x">3个月</i>
-          </button>
-      </div>
-
-      <div class="col-6 col-sm-4 col-md mb-3 mb-xl-0 text-center">
-          <button class="btn btn-block btn-outline-success duration" type="button" value="4">
-              <i class="fa fa-calendar text-primary fa-1x">4个月</i>
-          </button>
-      </div>
-
-      <div class="col-6 col-sm-4 col-md mb-3 mb-xl-0 text-center">
-          <button class="btn btn-block btn-outline-success duration" type="button" value="5">
-              <i class="fa fa-calendar text-primary fa-1x">5个月</i>
-          </button>
-      </div>
-  </div>
-  <br>
-
-  <div class="col text-center">
-      <button class="btn btn-success col-3" type="button" id="speed">
-         <i class="fa fa-bolt text-danger fa-3x">加速</i>
-
-      </button>
-  </div>
-
 @endsection
 
 @section('javascript')
-    <script>
+  <script src="{{ asset('assets/plugins/smart-wizard/js/jquery.smartWizard.min.js') }}"></script>
+  <script src="{{ asset('assets/plugins/range-slider/js/bootstrap-slider.min.js') }}"></script>
+
+  <script src="{{ asset('assets/js/pages/wizard-custom.js') }}" defer></script>
+
+  <script>
         $(document).ready(function () {
+            var slider = new Slider("#duration");
+            slider.on("slide", function (sliderValue) {
+                document.getElementById("durationVal").textContent = sliderValue;
+            });
+
             var id
             var duration
             $(".select-checked").click(function () {
@@ -83,15 +118,6 @@
                 $(this).addClass("btn-warning");
                 $(this).removeClass("btn-outline-warning");
                 id = $(this).val()
-            })
-
-            $(".duration").click(function () {
-                $(".duration").removeClass('btn-success');
-                $(".duration").addClass('btn-outline-success');
-                $(this).removeClass("btn-outline-success");
-                $(this).addClass("btn-success");
-
-                duration = $(this).val()
             })
 
 
@@ -112,7 +138,7 @@
                     data: {
                         id: id,
                         device_id: '{{ $device->id }}',
-                        duration: duration,
+                        duration: $('#duration').val(),
                         plugin_id: 2,
                         '_token': '{{ csrf_token() }}',
                     }
